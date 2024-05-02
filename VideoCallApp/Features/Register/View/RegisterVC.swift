8e28b12fee2d8b3bde6ad0c0e330a8dd8e5926af
Habip Yeşilyurt <yesilyurthabip@hotmail.com>
2024-05-03 02:36:12 +0300

    Create register view

---
+++ VideoCallApp/Features/Register/View/RegisterVC.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterVC.swift
+//  VideoCallApp
+//
+//  Created by Habip Yesilyurt on 3.05.2024.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
